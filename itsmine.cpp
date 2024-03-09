d4be653c45075ee9bd18fec33f5b3578d63d4052
Priyanshu Bansal <102759308+priyanshu233@users.noreply.github.com>
2024-03-09 19:23:17 +0530

    Update itsmine.cpp

--- itsmine.cpp
+++ itsmine.cpp
@@ -1,6 +1,16 @@
 /*
+to do ;-
 Policy based data structures
 rolling hash value
+
+// for leetcode contest
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
+
 
 
 
