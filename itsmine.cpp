e11c4043e091b3c18e1f637267d7a63421c72bff
Priyanshu Bansal <102759308+priyanshu233@users.noreply.github.com>
2024-05-05 17:06:12 +0530

    Update itsmine.cpp

--- itsmine.cpp
+++ itsmine.cpp
@@ -1,5 +1,19 @@
 
 /*
+binary exponentiation
+
+ll binpow(ll a,ll b){
+ll res = 1;
+  while(b>0){
+  if(b&1) res = res * a;
+  a = a*a;
+  b>>=1;
+  }
+
+  return res;
+}
+
+
 int mex(vi &arr)
 {
     int n = arr.size();
